21900b32c690e4a174250a5a7550acaef54e9a2e
50ShadesOfCode <kaktymail@gmail.com>
2023-08-11 16:46:56 +0300

    Split local data provider.

--- data/lib/providers/local_data_provider.dart
+++ data/lib/providers/local_data_provider.dart
@@ -9,36 +9,3 @@ abstract class LocalDataProvider {
 
   Future<bool> contains(String key);
 }
-
-class LocalDataProviderImpl implements LocalDataProvider{
-  @override
-  Future<bool> contains(String key) {
-    // TODO: implement contains
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> delete(String key) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> deleteAll() {
-    // TODO: implement deleteAll
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<String?> read(String key) {
-    // TODO: implement read
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> write({required String key, required String value}) {
-    // TODO: implement write
-    throw UnimplementedError();
-  }
-
-}
